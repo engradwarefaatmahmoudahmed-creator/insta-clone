f413e5221836499e834005272ddac10265715014
Radwa <engradwarefaatmahmoudahmed@yahoo.com>
2026-09-01 08:26:19 +0300

    Prepare project for deployment

--- insta_clone/settings.py
+++ insta_clone/settings.py
@@ -1,103 +1,111 @@
+import os
 from pathlib import Path
 
-
 BASE_DIR = Path(__file__).resolve().parent.parent
+SECRET_KEY = os.environ.get(
+'SECRET_KEY',
+'django-insecure-local-development-key'
+)
 
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-SECRET_KEY = 'django-insecure-m#os!)glxs#eq83qv1*u4n=6c=!15%fq6$65^-j!%t(6^^i((*'
+ALLOWED_HOSTS = os.environ.get(
+'ALLOWED_HOSTS',
+'127.0.0.1,localhost'
+).split(',')
 
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
+CSRF_TRUSTED_ORIGINS = [
+origin
+for origin in os.environ.get(
+'CSRF_TRUSTED_ORIGINS',
+''
+).split(',')
+if origin
+]
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
+'django.contrib.admin',
+'django.contrib.auth',
+'django.contrib.contenttypes',
+'django.contrib.sessions',
+'django.contrib.messages',
+'django.contrib.staticfiles',
 
-    'accounts.apps.AccountsConfig',
-    'posts.apps.PostsConfig',
-    'notifications.apps.NotificationsConfig',
+'accounts.apps.AccountsConfig',
+'posts.apps.PostsConfig',
+'notifications.apps.NotificationsConfig',
+
 ]
-
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+'django.middleware.security.SecurityMiddleware',
+'whitenoise.middleware.WhiteNoiseMiddleware',
+'django.contrib.sessions.middleware.SessionMiddleware',
+'django.middleware.common.CommonMiddleware',
+'django.middleware.csrf.CsrfViewMiddleware',
+'django.contrib.auth.middleware.AuthenticationMiddleware',
+'django.contrib.messages.middleware.MessageMiddleware',
+'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 
 ROOT_URLCONF = 'insta_clone.urls'
 
-
 TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+{
+'BACKEND': 'django.template.backends.django.DjangoTemplates',
 
-        'DIRS': [
-            BASE_DIR / 'templates',
+    'DIRS': [
+        BASE_DIR / 'templates',
+    ],
+
+    'APP_DIRS': True,
+
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
         ],
-
-        'APP_DIRS': True,
-
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
     },
-]
+},
 
+]
 
 WSGI_APPLICATION = 'insta_clone.wsgi.application'
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+'default': {
+'ENGINE': 'django.db.backends.sqlite3',
+'NAME': BASE_DIR / 'db.sqlite3',
+}
 }
 
-
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': (
-            'django.contrib.auth.password_validation.'
-            'UserAttributeSimilarityValidator'
-        ),
-    },
-    {
-        'NAME': (
-            'django.contrib.auth.password_validation.'
-            'MinimumLengthValidator'
-        ),
-    },
-    {
-        'NAME': (
-            'django.contrib.auth.password_validation.'
-            'CommonPasswordValidator'
-        ),
-    },
-    {
-        'NAME': (
-            'django.contrib.auth.password_validation.'
-            'NumericPasswordValidator'
-        ),
-    },
+{
+'NAME': (
+'django.contrib.auth.password_validation.'
+'UserAttributeSimilarityValidator'
+),
+},
+{
+'NAME': (
+'django.contrib.auth.password_validation.'
+'MinimumLengthValidator'
+),
+},
+{
+'NAME': (
+'django.contrib.auth.password_validation.'
+'CommonPasswordValidator'
+),
+},
+{
+'NAME': (
+'django.contrib.auth.password_validation.'
+'NumericPasswordValidator'
+),
+},
 ]
-
 
 LANGUAGE_CODE = 'en-us'
 
@@ -107,20 +115,31 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+BASE_DIR / 'static',
 ]
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
 
+STORAGES = {
+'default': {
+'BACKEND': 'django.core.files.storage.FileSystemStorage',
+},
+'staticfiles': {
+'BACKEND': (
+'whitenoise.storage.'
+'CompressedManifestStaticFilesStorage'
+),
+},
+}
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
 
 AUTH_USER_MODEL = 'accounts.User'
